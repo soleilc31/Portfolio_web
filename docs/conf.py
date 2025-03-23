952898b3b22ac4472219937e7ba240c6c401a5f8
Larry-lai-smarthon <larry.lai.smarthon@gmail.com>
2025-03-23 23:35:49 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ source_suffix = ['.rst', '.md']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
 
 html_favicon = "Icon.JPG"
